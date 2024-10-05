6c74d3b27d1a7deef7046c41c5fe9a2f993a9acd
Satya1512 <148025472+Satya1512@users.noreply.github.com>
2024-10-05 14:57:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -53,15 +53,14 @@ app.post('/api/contact', async (req, res) => {
     }
 });
 
-// Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('client/build'));
+    app.use(express.static(path.join(__dirname, 'client', 'build'))); // Adjust path as necessary
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
+
 
 // Start the server
 const PORT = process.env.PORT || 3000;
